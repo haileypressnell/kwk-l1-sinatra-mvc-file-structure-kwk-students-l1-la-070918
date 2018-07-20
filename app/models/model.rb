9b63ba3017066c6aa52dc8c2867dd8ab139315b0
haileypressnell <hailey.pressnell@yahoo.com>
2018-07-20 16:13:17 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,7 @@
-class Model
-  # Replace with Dog clas
-end
+ #THIS IS WHERE THE RUBY HAPPENS
+
+def get_fortune
+  fortunes_array = ["You will die alone!", "You suck!", "HA u ugly"]
+  return fortunes_array.sample
+end 
+puts get_fortune
